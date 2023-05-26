c9505c87e5c63326a3397a93db4bcf0ca24e0014
AkashDeoNU <103459323+AkashDeoNU@users.noreply.github.com>
2023-05-26 15:20:49 -0500

    installed tailwind & drafted navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 function NavBar() {
-  return <div className="App">Hello</div>;
+  return (
+    <ul className="flex">
+      <li class="mr-6">
+        <a class="text-blue-500 hover:text-blue-800" href="/">Active</a>
+      </li>
+      <li class="mr-6">
+        <a class="text-blue-500 hover:text-blue-800" href="/">Link</a>
+      </li>
+      <li class="mr-6">
+        <a class="text-blue-500 hover:text-blue-800" href="/">Link</a>
+      </li>
+      <li class="mr-6">
+        <a class="text-gray-400 cursor-not-allowed" href="/">Disabled</a>
+      </li>
+    </ul>
+  );
 }
 
 export default NavBar;
